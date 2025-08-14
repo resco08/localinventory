9d0bb9585d301d58ce696fc1eb624284d7f5cdf5
resco08 <rodolfpeterson@gmail.com>
2025-08-14 23:54:36 +0800

    SW added

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,57 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/customers.html',
-  '/invoice.html',
-  '/css/main-style.css',
-  '/css/invoice.css',
-  '/css/css/fontawesome.css',
-  '/css/css/brands.css',
-  '/css/css/solid.css',
-  '/js/customer.js',
-  '/js/invoice.js',
-  '/js/main-script.js'
-];
+const CACHE_NAME = "static_cache"
+const STATIC_ASSETS = [
+    './index.html',
+    './invoice.html',
+    './customers.html',
+    './js/main-script.js',
+    './js/invoice.js',
+    './js/customer.js',
+    './css/main-style.css',
+    './css/invoice.css',
+    './css/css/solid.css',
+    './css/css/brands.css',
+    './css/css/fontawesome.css',
+    './css/webfonts/fa-solid-900.ttf',
+    './css/webfonts/fa-solid-900.woff2'
+]
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache).catch((error) => {
-          console.error('Failed to cache some resources:', error);
-        });
-      })
-  );
-});
+async function preCache(){
+    const cache = await caches.open(CACHE_NAME)
+    return cache.addAll(STATIC_ASSETS)
+}
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse; // Serve from cache
-      }
-      return fetch(event.request); // Fetch from network if not cached
+self.addEventListener('install', event => {
+    console.log("[SW] installed");
+    event.waitUntil(preCache())
+})
+
+async function cleanupCache() {
+    const keys = await caches.keys()
+    const keysToDelete = keys.map(key => {
+        if (key !== CACHE_NAME) {
+            return caches.delete(key)
+        }
     })
-  );
-});
+
+    return Promise.all(keysToDelete)
+}
+
+self.addEventListener('activate', event => {
+    console.log("[SW] activated");
+    event.waitUntil(cleanupCache())
+})
+
+async function fetchAssets(event){
+    try {
+        const response = await fetch(event.request)
+        return response
+    } catch {
+        const cache = await caches.open(CACHE_NAME)
+        return cache.match(event.request)
+    }
+}
+
+self.addEventListener('fetch', event => {
+    console.log("[SW] fetched");
+    event.respondWith(fetchAssets(event))
+})
